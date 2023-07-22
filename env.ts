ed0fbc4b1aeb83af5d17d6d6818c4a21177bf1fc
Hugo Zhan <hugozhan@Hugos-MBP.attlocal.net>
2023-07-21 19:37:21 -0700

    updated icon and website title

--- env.ts
+++ env.ts
@@ -1,5 +1,5 @@
 export const EnvVars = {
-  SITE_NAME: 'My SaaS Startup',
+  SITE_NAME: 'Magnetic | Hyper Personailized Win-Back Campaigns',
   OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
   URL: 'https://next-saas-starter-ashy.vercel.app/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://gmail.us9.list-manage.com/subscribe/post?u=1361842a831068ce8f926d996&id=43e099b697',
